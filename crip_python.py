9bda0d41fbf72ae6c9912f3644acf36e6cee5974
David Acevedo <davidacevedomendieta2003@gmail.com>
2026-06-19 23:30:49 -0600

    Update crip_python.py

--- crip_python.py
+++ crip_python.py
@@ -46,7 +46,7 @@ dia = datetime.now().day
 
 mensaje = frases.get(dia, "Hoy es un buen día para ser feliz.")
 
-client = Client(account_SID, auth_TOKEN)
+client = Client(account_sid, auth_token)
 client.messages.create(
     from_= "whatsapp:+14155238886",
     to=    "whatsapp:+50583776225",
